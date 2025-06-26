8c77d54dd5eca329d6dcdf3ab0d9156d547b4334
Tu Nombreavril <avril@teams.com.mx>
2025-06-26 12:01:41 -0600

    next2

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 // File: stephaneavril/leo_api/LEO_API-7337efa878c219546557704899d19e82342974a0/next.config.js
 /** @type {import('next').NextConfig} */
-// const path = require('path'); // Remueve o comenta esta línea
-
 const nextConfig = {
   output: 'standalone',
-  // Remueve o comenta todo el bloque 'webpack' por ahora
-  // webpack: (config) => {
-  //   config.resolve.alias['@'] = path.join(__dirname, '');
-  //   return config;
-  // },
+  // **Asegúrate de incluir esta línea**
+  // Explícitamente establece assetPrefix a la ruta raíz.
+  // Esto ayuda a asegurar que todas las URL de los activos estáticos (JS, CSS)
+  // generadas por Next.js comiencen desde la raíz del dominio.
+  assetPrefix: '/',
 };
 
 module.exports = nextConfig;
