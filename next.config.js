b95453164b69691bedec573f305eafd8d0ac0435
Tu Nombreavril <avril@teams.com.mx>
2025-06-26 13:35:57 -0600

    runtime

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-// File: stephaneavril/leo_api/LEO_API-7337efa878c219546557704899d19e82342974a0/next.config.js
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // **Asegúrate de incluir esta línea**
-  // Explícitamente establece assetPrefix a la ruta raíz.
-  // Esto ayuda a asegurar que todas las URL de los activos estáticos (JS, CSS)
-  // generadas por Next.js comiencen desde la raíz del dominio.
-  assetPrefix: '/',
+  assetPrefix: '/frontend/', // Change this if your app is in a subdirectory on Render
 };
 
 module.exports = nextConfig;
