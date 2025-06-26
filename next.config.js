171861f8ef53d99f75c01b27d20daf9beb376244
Tu Nombreavril <avril@teams.com.mx>
2025-06-25 22:04:14 -0600

    todo 8

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path'); // Add this line at the top
+
 const nextConfig = {
   output: 'standalone',
+  // Add this webpack configuration block
+  webpack: (config) => {
+    // This maps '@' to your project's root directory
+    config.resolve.alias['@'] = path.join(__dirname, '');
+    return config;
+  },
 };
+
 module.exports = nextConfig;
