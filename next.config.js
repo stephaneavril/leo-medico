c4a67ccc3c4a67e9767fb081ba9812b462f01b0c
Tu Nombreavril <avril@teams.com.mx>
2025-06-26 11:52:46 -0600

    config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
+// File: stephaneavril/leo_api/LEO_API-7337efa878c219546557704899d19e82342974a0/next.config.js
 /** @type {import('next').NextConfig} */
-const path = require('path'); // Add this line at the top
+// const path = require('path'); // Remueve o comenta esta línea
 
 const nextConfig = {
   output: 'standalone',
-  // Add this webpack configuration block
-  webpack: (config) => {
-    // This maps '@' to your project's root directory
-    config.resolve.alias['@'] = path.join(__dirname, '');
-    return config;
-  },
+  // Remueve o comenta todo el bloque 'webpack' por ahora
+  // webpack: (config) => {
+  //   config.resolve.alias['@'] = path.join(__dirname, '');
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
