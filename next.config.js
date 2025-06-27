a60bdcf057f0227c01435d3d8653f72dc7e057f6
Tu Nombreavril <avril@teams.com.mx>
2025-06-27 07:16:55 -0600

    fix: quitar basePath/assetPrefix para producción

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
   output: 'standalone',
-  basePath: isProd ? '/frontend' : '',
-  assetPrefix: isProd ? '/frontend/' : '',
-  // …otras opciones que tengas
 };
